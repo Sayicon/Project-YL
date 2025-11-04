8beae00aeb59c5df8f3abbfdb135d2ebb846721e
Mustafa Mehmet Aslandağ <mezoxy@outlook.com>
2025-11-05 00:30:34 +0300

    yeni map design
    map güncellendi, ışık ayarlarıyla oynandı, sis eklendi ama şuanlık kapalı konumda

---
+++ Project YL/Assets/Scripts/LevelGeneration.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class LevelGeneration : MonoBehaviour
+{
+    public GameObject[] objects;
+    void Start()
+    {
+        int rand = Random.Range(0, objects.Length);
+        Instantiate(objects[rand], transform.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
